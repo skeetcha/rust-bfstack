4dc2daf25f2f52b76987927b7bbc0ec83f9e44db
Cass Unterholzner <cityboundforest@gmail.com>
2023-09-03 09:30:49 -0500

    Adding final bracket handling code

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,26 @@ use std::io::{self, Read};
 use std::io::Write;
 
 fn interpret(input: &String, stack: &mut Vec<i64>) {
-    let mut loop_loc = -1;
+    let mut prog_loc = 0;
+    let mut locs: Vec<usize> = Vec::new();
 
-    for (prog_loc, op) in input.chars().enumerate() {
-        match op {
+    while prog_loc < input.len() {
+        match input.chars().nth(prog_loc).unwrap() {
             '>' => stack.push(0),
             '<' => {
-                let _ = stack.pop().unwrap();
+                if stack.len() > 0 {
+                    let _ = stack.pop().unwrap();
+                } else {
+                    println!("Stack is empty");
+                }
             },
-            '.' => println!("{}", stack.last().unwrap()),
+            '.' => {
+                if stack.len() > 0 {
+                    print!("{}", char::from(((*stack.last().unwrap()) % 255) as u8));
+                } else {
+                    println!("Stack is empty");
+                }
+            },
             ',' => {
                 let mut buf: [u8; 1] = [b'\0'];
                 io::stdin().read_exact(&mut buf).expect("failed to read character");
@@ -23,13 +34,31 @@ fn interpret(input: &String, stack: &mut Vec<i64>) {
                 *stack.last_mut().unwrap() -= 1;
             },
             '[' => {
+                if *stack.last().unwrap() == 0 {
+                    let mut matches = 1;
 
+                    while matches != 0 {
+                        prog_loc += 1;
+
+                        if prog_loc == input.len() {
+                            break;
+                        } else if input.chars().nth(prog_loc).unwrap() == '[' {
+                            matches += 1;
+                        } else if input.chars().nth(prog_loc).unwrap() == ']' {
+                            matches -= 1;
+                        }
+                    }
+                } else {
+                    locs.push(prog_loc);
+                }
             },
             ']' => {
-
+                prog_loc = locs.pop().unwrap() - 1;
             },
             _ => ()
         };
+
+        prog_loc += 1;
     }
 }
 
